b741eaf7f3b184b0adeed7ea9cd84b688b011541
John Mascagni <johnmascagni@gmail.com>
2020-08-06 15:00:14 -0500

    Fix makesig again

--- ida_writer.py
+++ ida_writer.py
@@ -56,6 +56,7 @@ def makesig(func):
 		if info is None:
 			return None
 
+		done = 0
 		if len(info.ops) == 1:
 			if info.ops[0].type == ida_ua.o_near or info.ops[0].type == ida_ua.o_far:
 				if hex(idc.get_wide_byte(addr)) == 0x0F: 	# Two-byte instruction
