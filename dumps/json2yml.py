566b8bb72bdc6d7f6914051fee6d8015ec035e50
John Mascagni <johnmascagni@gmail.com>
2020-08-24 06:52:09 -0500

    Dumps with fixed makesig

--- dumps/json2yml.py
+++ dumps/json2yml.py
@@ -3,11 +3,14 @@ import yaml
 import sys
 
 def main():
-	with open(sys.argv[1], "r") as f:
-		j = json.load(f)
+	for i in xrange(len(sys.argv)):
+		if i == 0:
+			continue
+		with open(sys.argv[i], "r") as f:
+			j = json.load(f)
 
-	with open(sys.argv[1].replace("json", "yml"), "w") as f:
-		yaml.safe_dump(j, f, default_flow_style = False, width = 999999)
+		with open(sys.argv[i].replace("json", "yml"), "w") as f:
+			yaml.safe_dump(j, f, default_flow_style = False, width = 999999)
 
 if __name__ == "__main__":
 	main()
